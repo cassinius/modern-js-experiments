798f61516400fae67e09bde6e92d53cf38f022d2
Bernd Malle <bernd.malle@gmail.com>
2024-02-04 06:58:36 +0800

    add ws_client project with Svelte 5 (Runes!)

---
+++ ws/ws_client_svelte/src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	let dice = $state<number>(0);
+	let rolls = $state<number[]>([]);
+
+	function draw() {
+		dice = Math.floor(Math.random() * 6) + 1;
+		// NOTE: interesting to see how the state is updated *reactively*
+		rolls[rolls.length] = dice;
+	}
+
+	function reset() {
+		dice = 0;
+		rolls = [];
+	}
+</script>
+
+<section style="padding-top: 50px;">
+	<button on:click={draw}> Let's draw </button>
+	<button on:click={reset}> Let's restart </button>
+</section>
+
+<section>
+	<h2 style="text-align: center">
+		{dice == 0 ? "Let's start rolling" : dice}
+	</h2>
+
+	{#each rolls as t, index}
+		<p style="text-align: center">
+			Rolling number {index + 1}, the dice says {t}
+		</p>
+	{/each}
+</section>
